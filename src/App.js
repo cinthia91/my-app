a3bad7c0a895780372550c91bd74d63eca9b104f
Cinthia Ortiz <cinthia2208@gmail.com>
2023-01-16 14:18:52 -0300

    my-app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> MODIFICACION</h1>
+        <p> modificacion</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
