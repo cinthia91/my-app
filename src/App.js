050e8780bd68f564bfa514dcbd452bf285de5be5
Cinthia Ortiz <cinthia2208@gmail.com>
2023-01-16 15:49:37 -0300

    PrimeraVariacion

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <h1> MODIFICACION</h1>
         <p> modificacion</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
